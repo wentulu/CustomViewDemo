4aa9301bc4277e4324de3623ec321598350ec658
Ruiz <971961844@qq.com>
2018-11-02 17:11:34 +0800

    TitleView Log Switcher

--- app/src/main/java/cn/stu/cusview/ruiz/customeviewdemo/view/TitleView.java
+++ app/src/main/java/cn/stu/cusview/ruiz/customeviewdemo/view/TitleView.java
@@ -14,10 +14,11 @@ import android.view.View;
 
 import cn.stu.cusview.ruiz.customeviewdemo.R;
 
-public class TitleView extends View{
+public class TitleView extends View {
 
     private static final String Tag = TitleView.class.getSimpleName();
 
+    private static final boolean DEBUG = false;
 
     private String mTitleStr;//
     private int mTitleColor;
@@ -30,16 +31,16 @@ public class TitleView extends View{
     private float density;
 
     public TitleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public TitleView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public TitleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs , defStyleAttr);
+        init(attrs, defStyleAttr);
     }
 
     /***
@@ -47,33 +48,37 @@ public class TitleView extends View{
      * */
     private void init(AttributeSet attributeSet, int defSty) {
         density = getResources().getDisplayMetrics().density;
-        Log.e(Tag,"Display density="+density);
+        Log.e(Tag, "Display density=" + density);
         int count = attributeSet.getAttributeCount();
-        for (int poc = 0; poc<count; poc++){
-            Log.e(Tag,"attrName="+attributeSet.getAttributeName(poc)+"  attrValue="+attributeSet.getAttributeValue(poc));
+        for (int poc = 0; poc < count; poc++) {
+            if (DEBUG)
+                Log.e(Tag, "attrName=" + attributeSet.getAttributeName(poc) + "  attrValue=" + attributeSet.getAttributeValue(poc));
         }
-        TypedArray array = getContext().getTheme().obtainStyledAttributes(attributeSet, R.styleable.TitleView,defSty,0);
+        TypedArray array = getContext().getTheme().obtainStyledAttributes(attributeSet, R.styleable.TitleView, defSty, 0);
         int arrCount = array.getIndexCount();
-        for (int index = 0; index<arrCount; index++){
+        for (int index = 0; index < arrCount; index++) {
             int attr = array.getIndex(index);
-            switch (attr){
+            switch (attr) {
                 case R.styleable.TitleView_title:
                     mTitleStr = array.getString(index);
-                    Log.e(Tag,"titleStr="+mTitleStr);
+                    if (DEBUG)
+                        Log.e(Tag, "titleStr=" + mTitleStr);
                     break;
                 case R.styleable.TitleView_titleColor:
-                    mTitleColor = array.getColor(index,Color.BLUE);
-                    Log.e(Tag,"titleColor="+mTitleColor+"");
+                    mTitleColor = array.getColor(index, Color.BLUE);
+                    if (DEBUG)
+                        Log.e(Tag, "titleColor=" + mTitleColor + "");
                     break;
                 case R.styleable.TitleView_titleSize:
-                    mTitleSize = array.getDimensionPixelSize(index, (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,16,getResources().getDisplayMetrics()));
-                    Log.e(Tag,"titleSize="+mTitleSize);
+                    mTitleSize = array.getDimensionPixelSize(index, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    if (DEBUG)
+                        Log.e(Tag, "titleSize=" + mTitleSize);
                     break;
             }
         }
 
 
-        if (mPaint == null){
+        if (mPaint == null) {
             mPaint = new Paint();
             mPaint.setTextSize(mTitleSize);
             mPaint.setStyle(Paint.Style.STROKE);
@@ -82,46 +87,50 @@ public class TitleView extends View{
         }
 
         textRect = new Rect();
-        mPaint.getTextBounds(mTitleStr,0,mTitleStr.length(),textRect);
-        float width = mPaint.measureText(mTitleStr,0,mTitleStr.length());
-        textRect.left=0;
-        textRect.right = (int)(width+0.5f);
-        textRect.top=0;
+        mPaint.getTextBounds(mTitleStr, 0, mTitleStr.length(), textRect);
+        float width = mPaint.measureText(mTitleStr, 0, mTitleStr.length());
+        textRect.left = 0;
+        textRect.right = (int) (width + 0.5f);
+        textRect.top = 0;
         textRect.bottom = mTitleSize;
         baseTextLine = mPaint.getFontMetrics().ascent;
-        Log.e(Tag,"text  top:right:bottom:left="+textRect.top+":"+textRect.right+":"+textRect.bottom+":"+textRect.left);
-        Log.e(Tag,"Text width:height="+textRect.width()+":"+textRect.height());
+        if (DEBUG) {
+            Log.e(Tag, "text  top:right:bottom:left=" + textRect.top + ":" + textRect.right + ":" + textRect.bottom + ":" + textRect.left);
+            Log.e(Tag, "Text width:height=" + textRect.width() + ":" + textRect.height());
 
 
-        Log.e(Tag,"Display width:height="+getResources().getDisplayMetrics().widthPixels+":"+getResources().getDisplayMetrics().heightPixels);
-
+            Log.e(Tag, "Display width:height=" + getResources().getDisplayMetrics().widthPixels + ":" + getResources().getDisplayMetrics().heightPixels);
+        }
     }
 
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int height = getSize(heightMeasureSpec,true);
-        int width = getSize(widthMeasureSpec,false);
-        Log.e(Tag,"measured Width:Height="+width+":"+height);
-        setMeasuredDimension(width,height);
+        int height = getSize(heightMeasureSpec, true);
+        int width = getSize(widthMeasureSpec, false);
+        if (DEBUG)
+            Log.e(Tag, "measured Width:Height=" + width + ":" + height);
+        setMeasuredDimension(width, height);
     }
 
 
-    private int getSize(int measureSpec ,boolean height){
+    private int getSize(int measureSpec, boolean height) {
         int mode = MeasureSpec.getMode(measureSpec);
         int size = MeasureSpec.getSize(measureSpec);
-        if (height){
-            Log.e(Tag,"Measuring height="+size);
-        }else {
-            Log.e(Tag,"Measuring Width="+size);
+        if (DEBUG) {
+            if (height) {
+                Log.e(Tag, "Measuring height=" + size);
+            } else {
+                Log.e(Tag, "Measuring Width=" + size);
+            }
         }
-        if (mode==MeasureSpec.EXACTLY){
+        if (mode == MeasureSpec.EXACTLY) {
             return size;
-        }else {
+        } else {
             if (height) {
                 return textRect.height() + getPaddingBottom() + getPaddingTop();
-            }else {
+            } else {
                 return textRect.width() + getPaddingLeft() + getPaddingRight();
             }
         }
@@ -131,7 +140,7 @@ public class TitleView extends View{
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawText(mTitleStr,(getWidth()-textRect.width())/2,(getHeight()-textRect.height())/2-(baseTextLine+mPaint.getFontMetrics().bottom-density) ,mPaint);
+        canvas.drawText(mTitleStr, (getWidth() - textRect.width()) / 2, (getHeight() - textRect.height()) / 2 - (baseTextLine + mPaint.getFontMetrics().bottom - density), mPaint);
 
     }
 }
