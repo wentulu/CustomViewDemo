ab206d2379ebee649d4626bc4c02b032799e8f1e
Ruiz <971961844@qq.com>
2019-01-13 19:59:15 +0800

    contentprovider resolver

--- app/src/main/java/cn/stu/cusview/ruiz/customeviewdemo/provider/MyContentProvider.java
+++ app/src/main/java/cn/stu/cusview/ruiz/customeviewdemo/provider/MyContentProvider.java
@@ -9,6 +9,24 @@ import android.net.Uri;
 
 import cn.stu.cusview.ruiz.customeviewdemo.db.MyDataBaseHelper;
 
+/**
+ * 取值使用的方法，在另外一个app，目前只是实现了查询
+ * <p>
+ * private String uri = "content://cn.stu.ruiz.custom/Book";
+ * <p>
+ * <p>
+ * Cursor cursor = getContentResolver().query(Uri.parse(uri),null,null,null,null);
+ * if (cursor.moveToFirst()){
+ * StringBuilder stringBuilder = new StringBuilder();
+ * do {
+ * String bookName = cursor.getString(cursor.getColumnIndex("bookName"));
+ * String author = cursor.getString(cursor.getColumnIndex("author"));
+ * stringBuilder.append("["+bookName+":"+author+"]^");
+ * }while (cursor.moveToNext());
+ * book_tv.setText(stringBuilder.toString());
+ * }
+ */
+
 public class MyContentProvider extends ContentProvider {
 
     MyDataBaseHelper mMyDataBaseHelper;
@@ -43,7 +61,7 @@ public class MyContentProvider extends ContentProvider {
 
     @Override
     public String getType(Uri uri) {
-        switch (uriMatcher.match(uri)){
+        switch (uriMatcher.match(uri)) {
             case BOOK_DIR:
                 return "vnd.android.cursor.dir/vnd.cn.stu.ruiz.custom.provider.Book";
 
@@ -62,9 +80,9 @@ public class MyContentProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
         SQLiteDatabase database = mMyDataBaseHelper.getWritableDatabase();
-        switch (uriMatcher.match(uri)){
+        switch (uriMatcher.match(uri)) {
             case BOOK_DIR:
-               return database.query("Book",null,null,null,null,null,null);
+                return database.query("Book", null, null, null, null, null, null);
         }
 
         return null;
